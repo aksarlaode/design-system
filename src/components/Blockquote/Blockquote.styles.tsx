24c86fadfac9970eb4102a1a1d3991f4f1e41d0c
bahutara <108091324+bahutara@users.noreply.github.com>
2022-10-13 12:24:05 +0800

    Edit

--- src/components/Blockquote/Blockquote.styles.tsx
+++ src/components/Blockquote/Blockquote.styles.tsx
@@ -1,67 +1,50 @@
-import { styled } from 'src/lib/stitches.config';
+import { styled } from "src/lib/stitches.config";
 
-export const StyledBlockquote = styled('blockquote', {
-  fontWeight: '$2',
-  lineHeight: '$relaxed',
-  letterSpacing: '$tight',
-  fontSize: '$4',
-  color: 'var(--laodeaksar-colors-typeface-secondary)',
-  position: 'relative',
-  margin: '0 0 24px 0',
-  isolation: 'isolate',
+export const StyledBlockquote = styled("blockquote", {
+  fontWeight: "$2",
+  lineHeight: "$relaxed",
+  letterSpacing: "$tight",
+  fontSize: "$4",
+  color: "var(--laodeaksar-colors-typeface-secondary)",
+  position: "relative",
+  margin: "0 0 24px 0",
+  isolation: "isolate",
 
-  '@lg': {
-    fontSize: '$6',
+  "@lg": {
+    fontSize: "$6"
   },
 
   p: {
-    marginBottom: 0,
+    marginBottom: 0
   },
 
   cite: {
-    letterSpacing: '$normal',
-    fontSize: '$2',
+    letterSpacing: "$normal",
+    fontSize: "$1",
 
-    '@lg': {
-      fontSize: '$3',
+    "@lg": {
+      fontSize: "$3"
     },
+
+    "&:before": {
+      content: '"~ "'
+    }
   },
 
-  '&::before': {
-    content: 'open-quote',
-    position: 'absolute',
-    fontSize: '20rem',
-    fontWeight: 900,
-    fontFamily: 'serif',
-    color: 'var(--laodeaksar-colors-typeface-primary)',
-    zIndex: '-1',
-    left: -15,
-    top: -25,
-
-    '@md': {
-      left: -85,
-      top: -15,
-    },
-
-    '@lg': {
-      top: 0,
-    },
-  },
-
-  '.quotation': {
+  ".quotation": {
     opacity: 0.1,
     size: 80,
-    position: 'absolute',
+    position: "absolute",
     left: -15,
     top: -25,
 
-    '@md': {
+    "@md": {
       left: -85,
-      top: -15,
+      top: -15
     },
 
-    '@lg': {
-      top: 0,
-    },
-  },
+    "@lg": {
+      top: 0
+    }
+  }
 });
